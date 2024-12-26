c80f1345e9918b280e44fcc2f0140a26739242af
Marius <mariusfagerland@yahoo.no>
2024-12-26 13:44:48 +0100

    Add api call to frontend

--- src/features/wines/Wines.tsx
+++ src/features/wines/Wines.tsx
@@ -1,20 +1,23 @@
+import { useSignal } from "@preact/signals"
 import { Wine } from "./types"
 import { WinesView } from "./WinesView"
+import { useEffect } from "preact/hooks"
+
+const apiUrl = 'https://bottle-tracker-go-api-production.up.railway.app/wines'
 
 export const Wines = () => {
-	const wines: Wine[] = [
-		{
-			name: 'Chardonnay',
-		},
-		{
-			name: 'Merlot',
-		},
-		{
-			name: 'Cabernet Sauvignon',
-		},
-	]
+	const wines = useSignal<Wine[]>([])
+
+	useEffect(() => {
+		const getWines = async () => {
+			const response = await fetch(apiUrl)
+			const data = await response.json()
+			wines.value = data
+		}
+		getWines()
+	}, [])
 
 	return (
-		<WinesView wines={wines} />
+		<WinesView wines={wines.value} />
 	)
 }
